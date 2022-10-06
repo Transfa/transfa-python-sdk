e758c62c0b55d247f97a7fb12b11fab53dea31ea
Ederene20 <atinhoruben@gmail.com>
2022-10-06 22:16:06 +0100

    refac: little refactoring

--- transfa/webhook.py
+++ transfa/webhook.py
@@ -2,9 +2,9 @@ import hmac
 import hashlib
 
 
-def verify_webhook(api_secret_key, data, hmac_header):
+def verify_webhook(secret_key, body, request_header):
 
-    signature = hmac.new(api_secret_key.encode('utf-8'), data, digestmod=hashlib.sha512)
+    signature = hmac.new(secret_key.encode('utf-8'), body, digestmod=hashlib.sha512)
     signature = signature.hexdigest()
 
-    return hmac.compare_digest(signature, hmac_header.encode('utf-8'))
+    return hmac.compare_digest(signature, request_header.encode('utf-8'))
