02a985b6c4ae9e9885e8bb0284686d33b96d6125
Ederene20 <atinhoruben@gmail.com>
2022-10-09 02:01:27 +0100

    feat: created a function to sign the body of the request and wrapped functions with a class

--- transfa/webhook.py
+++ transfa/webhook.py
@@ -1,10 +1,31 @@
 import hmac
 import hashlib
+from datetime import datetime
 
 
-def verify_webhook(secret_key, body, request_header):
+class Webhook:
+    def sign_body(self, secret_key, body):
 
-    signature = hmac.new(secret_key.encode('utf-8'), body, digestmod=hashlib.sha512)
-    signature = signature.hexdigest()
+        if not isinstance(body, bytes):
+            body = body.encode("utf-8")
 
-    return hmac.compare_digest(signature, request_header.encode('utf-8'))
+        signature = hmac.new(secret_key.encode('utf-8'), body, digestmod=hashlib.sha512)
+
+        return signature.hexdigest()
+
+    def verify_webhook(self, secret_key, body, request_header):
+
+        signature = self.sign_body(secret_key, body)
+        verified = hmac.compare_digest(signature, request_header.encode('utf-8'))
+
+        if not verified:
+            return False, None
+
+        data = body
+        data["created"] = datetime.fromtimestamp(data["created"]).strftime("%m/%d/%Y %H:%M:%S")
+        data["update"] = datetime.fromtimestamp(data["update"]).strftime("%m/%d/%Y %H:%M:%S")
+
+        return verified, data
+
+
+webhook = Webhook()
