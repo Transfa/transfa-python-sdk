1c56598730cc0534a448d3d0096d52d730cdfebb
Ederene20 <atinhoruben@gmail.com>
2022-12-03 09:58:42 +0100

    - feat: added webhook payload as pytest fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,3 +17,23 @@ def payment(payments):
     :return: a payment object
     """
     return payments[0]
+
+
+@pytest.fixture()
+def valid_webhook_payload():
+    """
+    :return: a valid webhook payload
+    """
+
+    with open("tests/valid_webhook_payload.json") as f:
+        return json.load(f)
+
+
+@pytest.fixture()
+def invalid_webhook_payload():
+    """
+    :return: an invalid webhook payload
+    """
+
+    with open("tests/invalid_webhook_payload.json") as f:
+        return json.load(f)
