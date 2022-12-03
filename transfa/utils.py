09dfb105e1e453c5f40d41ffa3013ce9c7f88e5c
Ederene20 <atinhoruben@gmail.com>
2022-12-03 09:57:56 +0100

    -feat: added a helper function to generate token string

--- transfa/utils.py
+++ transfa/utils.py
@@ -1,5 +1,23 @@
 # utils and helpers
+import secrets
 
 
 def get_default_error_log_message(response):
     return f"Response has failed with status {response.status_code} => {response.text}"
+
+
+RANDOM_STRING_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+
+def get_random_string(length, allowed_chars=RANDOM_STRING_CHARS):
+    """
+    Return a securely generated random string.
+
+    The bit length of the returned value can be calculated with the formula:
+        log_2(len(allowed_chars)^length)
+
+    For example, with default `allowed_chars` (26+26+10), this gives:
+      * length: 12, bit length =~ 71 bits
+      * length: 22, bit length =~ 131 bits
+    """
+    return "".join(secrets.choice(allowed_chars) for i in range(length))
